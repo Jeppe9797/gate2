5470c547d54953e148e36c01413ef97fd0b7c1ae
Jeppe9797 <jeppe.stie.svendsen@gmail.com>
2025-07-20 13:42:20 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,1 @@
-// src/utils.js
 
-/** Formaterer JS Date til “HH:MM” dansk stil */
-export function formatTime(date) {
-  return date.toLocaleTimeString("da-DK", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-}
-
-/** Beregn hvor mange minutter til startTime, eller “Starter nu” */
-export function calcStartsIn(startTime) {
-  const now = new Date();
-  const diffMin = Math.round((startTime - now) / 60000);
-  return diffMin > 0 ? `Starter om ${diffMin} min` : "Starter nu";
-}
